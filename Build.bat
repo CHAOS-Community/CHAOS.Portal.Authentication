e855318df142d162e4c48467e2d454c82689b697
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2012-04-23 13:07:05 +0200

    Added build script for secure cookie module Modified build script for email password module

--- Build.bat
+++ Build.bat
@@ -9,4 +9,12 @@ echo Merging EmailPassword
 
 tools\ILMerge\ILMerge.exe /out:build\CHAOS.Portal.Authentication.EmailPasswordModule.dll /lib:C:\Windows\Microsoft.NET\Framework64\v4.0.30319 /targetplatform:v4,C:\Windows\Microsoft.NET\Framework64\v4.0.30319 /lib:lib\ %files%
 
+set files=
+
+for %%i in (.\bin\SecureCookie\*.dll) do (set files=!files!%%~i )
+
+echo Merging SecureCookie
+
+tools\ILMerge\ILMerge.exe /out:build\CHAOS.Portal.Authentication.SecureCookiedModule.dll /lib:C:\Windows\Microsoft.NET\Framework64\v4.0.30319 /targetplatform:v4,C:\Windows\Microsoft.NET\Framework64\v4.0.30319 /lib:lib\ %files%
+
 echo Done
