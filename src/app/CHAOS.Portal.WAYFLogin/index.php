15dbc9679eefd22bbc0967917d94f655e56715ad
Jacob Poul Richardt <spam@poulrichardt.dk>
2013-09-17 16:08:37 +0200

    Added Wayf login project and extension.

---
+++ src/app/CHAOS.Portal.WAYFLogin/index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Wayf Login Test</title>
+    </head>
+    <body>
+        <h1>Wayf Login Test</h1>
+        <p>
+            <?php
+                echo "Hello World!";
+            ?>
+        </p>
+        <p>
+            Wayf login test for Larm.fm
+        </p>
+    </body>
+</html>
---
+++ src/app/Chaos.Portal.Authentication/Extension/Wayf.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Chaos.Portal.Core;
+using Chaos.Portal.Core.Data.Model;
+using Chaos.Portal.Core.Extension;
+
+namespace Chaos.Portal.Authentication.Extension
+{
+	public class Wayf : AExtension
+	{
+		public Wayf(IPortalApplication portalApplication) : base(portalApplication)
+		{
+		}
+
+		public ScalarResult Login(string wayfId)
+		{
+			throw new NotImplementedException();
+
+			return new ScalarResult(1);
+		}
+	}
+}
