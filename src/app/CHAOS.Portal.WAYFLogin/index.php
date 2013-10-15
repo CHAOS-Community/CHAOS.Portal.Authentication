2fbe6744355176ab0aa70bb4c2f3860f79d079e6
Jacob Poul Richardt <spam@poulrichardt.dk>
2013-10-15 16:12:26 +0200

    Added code to use WAYF SP and call portal.

--- src/app/CHAOS.Portal.WAYFLogin/index.php
+++ src/app/CHAOS.Portal.WAYFLogin/index.php
@@ -1,3 +1,7 @@
+<?php
+	require_once('../../../tools/simplesamlphp-1.11.0/lib/_autoload.php');
+	$API_PATH = "https://myapi.com";
+?>
 <html>
     <head>
         <title>Wayf Login Test</title>
@@ -5,12 +9,51 @@
     <body>
         <h1>Wayf Login Test</h1>
         <p>
-            <?php
-                echo "Hello World!";
-            ?>
-        </p>
-        <p>
-            Wayf login test for Larm.fm
+		<?php	   
+			$simpleSaml = new SimpleSAML_Auth_Simple("LARM.fm SP");
+		   
+			if(!isset($_REQUEST["sessionGuid"]))
+				print("Parameter sessionGuid not set");
+			else
+			{
+				$simpleSaml->requireAuth();
+				
+				$sessionGuid = $_REQUEST["sessionGuid"];
+				$attributes = $simpleSaml->getAttributes();
+				
+				$parameters = array(
+					'format' => 'json2',
+					'sessionGuid' => $sessionGuid,
+					'wayfId' => $attributes['eduPersonTargetedID'][0],
+					'givenName' => $attributes['GivenName'][0],
+					'surName' => $attributes['SurName'][0],
+					'commonName' => $attributes['CommonName'][0],
+					);
+				
+				$ch = curl_init();
+
+				curl_setopt($ch, CURLOPT_URL, $API_PATH + "/v6/Wayf/Login");
+				curl_setopt($ch, CURLOPT_POST, 1);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+				$rawResponse = curl_exec ($ch);
+
+				curl_close ($ch);
+				
+				$response = json_decode($rawResponse);
+				
+				if($response == null)
+					print("Failed to parse Portal response");
+				else
+				{
+					if($response["Error"]["Message"] != null)
+						print("Portal authentication failed: " + $response["Error"]["Message"]);
+					
+					print("Session authenticated");
+				}
+			}
+		?>
         </p>
     </body>
 </html>
