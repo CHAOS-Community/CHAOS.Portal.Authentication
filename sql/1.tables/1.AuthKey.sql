e1cacd257bd7f932ac2fa106765acecf8e8dcbd9
Jacob Poul Richardt <spam@poulrichardt.dk>
2013-11-15 15:16:53 +0100

    Removed test file. Fixed AuthKey table.

--- sql/1.tables/1.AuthKey.sql
+++ sql/1.tables/1.AuthKey.sql
@@ -3,7 +3,7 @@
   `Token` VARCHAR(255) NOT NULL,
   `UserGuid` BINARY(16) NOT NULL,
   `Name` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`Key`),
+  PRIMARY KEY (`Token`),
   UNIQUE INDEX `Token_UNIQUE` (`Token` ASC),
   UNIQUE INDEX `UserGuid_Name_Unique` (`UserGuid` ASC, `Name` ASC)
 );
