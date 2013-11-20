23ae34486eb56a0077d844807bc12d2233da67ba
Jesper Fyhr Knudsen <Jesper@fyhr.dk>
2013-11-20 15:19:20 +0100

    Remvoed Given and Sur name

--- sql/1.tables/1.WayfProfile.sql
+++ sql/1.tables/1.WayfProfile.sql
@@ -2,8 +2,6 @@
 (
 	UserGuid			binary(16) NOT NULL,
 	WayfId				varchar(256) NOT NULL,
-	GivenName		varchar(1024) NOT NULL,
-	SurName			varchar(1024) NOT NULL,
 	DateCreated		datetime NOT NULL,
 	DateModified		datetime DEFAULT NULL,
 	PRIMARY KEY (UserGuid),
