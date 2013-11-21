8c529f0354d0dc4783471d2d938a26b4fa64a832
Jesper Fyhr Knudsen <Jesper@fyhr.dk>
2013-11-21 17:57:39 +0100

    Added Facebook call to translate the signed request into a user Id (ALLY-27)

--- src/app/Chaos.Portal.Authentication/Facebook/FacebookClient.cs
+++ src/app/Chaos.Portal.Authentication/Facebook/FacebookClient.cs
@@ -1,17 +1,36 @@
 ﻿namespace Chaos.Portal.Authentication.Facebook
 {
+    using System.Web.UI;
     using Configuration;
 
     public class FacebookClient : IFacebookClient
     {
-        public FacebookClient(FacebookSettings facebook)
+        public FacebookClient(FacebookSettings settings)
         {
-            
+            Settings = settings;
         }
 
+        private FacebookSettings Settings { get; set; }
+        
         public ulong GetUser(string signedRequest)
         {
-            return 0;
+            var client = new global::Facebook.FacebookClient
+            {
+                AppId = Settings.AppId,
+                AppSecret = Settings.AppSecret
+            };
+
+//            var result = client.Get("oauth/access_token", new
+//            {
+//                client_id = client.AppId,
+//                client_secret = client.AppSecret,
+//                grant_type = "client_credentials"
+//            });
+//            client.AccessToken = result.access_token;
+
+            var userId = (client.ParseSignedRequest(signedRequest) as dynamic).user_id;
+
+            return userId;
         }
     }
 }
