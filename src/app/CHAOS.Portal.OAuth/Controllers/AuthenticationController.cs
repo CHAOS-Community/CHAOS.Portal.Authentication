df0b990067412585e796ac4a48779bf05594416f
Jacob Poul Richardt <spam@poulrichardt.dk>
2014-03-10 15:38:28 +0100

    Added Portal interaction to oauth login.

--- src/app/CHAOS.Portal.OAuth/Controllers/AuthenticationController.cs
+++ src/app/CHAOS.Portal.OAuth/Controllers/AuthenticationController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Configuration;
 using System.Web.Mvc;
+using CHAOS.Portal.Client.Extensions;
+using CHAOS.Portal.Client.Standard;
 using DotNetAuth.OAuth2;
 
 namespace CHAOS.Portal.OAuth.Controllers
@@ -12,12 +14,19 @@ namespace CHAOS.Portal.OAuth.Controllers
 		private ApplicationCredentials _credentials;
 		private OAuth2SessionStateManager _oauth2StateManager;
 	    private ProviderConfiguration _providerConfiguration;
+	    private PortalConfiguration _portalConfiguration;
 	    private GenericProvider _provider;
+	    private Guid _sessionGuid;
 
 		protected override System.IAsyncResult BeginExecute(System.Web.Routing.RequestContext requestContext, System.AsyncCallback callback, object state)
 		{
+			if (requestContext.HttpContext.Request.QueryString["sessionGuid"] == null) throw new Exception("Missing sessiongGuid parameter");
+
 			_oauth2StateManager = new OAuth2SessionStateManager(requestContext.HttpContext.Session);
-			_providerConfiguration = (ProviderConfiguration)ConfigurationManager.GetSection("ProviderConfigurationGroup/ProviderConfiguration");
+			_providerConfiguration = (ProviderConfiguration)ConfigurationManager.GetSection("OAuthConfiguration/ProviderConfiguration");
+			_portalConfiguration = (PortalConfiguration)ConfigurationManager.GetSection("OAuthConfiguration/PortalConfiguration");
+			_sessionGuid = Guid.Parse(requestContext.HttpContext.Request.QueryString["sessionGuid"]);
+
 			_credentials = new ApplicationCredentials
 			{
 				AppId = _providerConfiguration.ClientId,
@@ -48,9 +57,16 @@ namespace CHAOS.Portal.OAuth.Controllers
 
 			LoginSuccessful = response.Result.Succeed;
 
-			//var client = new PortalClient();
+			if (LoginSuccessful)
+			{
+				var oAuthId = "";
+				var email = "";
 
-
+				var client = new PortalClient();
+				client.Session().Create().Synchronous().ThrowError(); ;
+				client.AuthKey().Login(_portalConfiguration.AuthKeyToken).Synchronous().ThrowError();
+				client.OAuth().Login(oAuthId, email, _sessionGuid).Synchronous().ThrowError();
+			}
 
 			return View();
 		}
---
+++ src/app/CHAOS.Portal.OAuth/Controllers/PortalConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+
+namespace CHAOS.Portal.OAuth.Controllers
+{
+	public class PortalConfiguration : ConfigurationSection
+	{
+		[ConfigurationProperty("AuthKeyToken", IsRequired = true)]
+		public string AuthKeyToken
+		{
+			get { return (string)this["AuthKeyToken"]; }
+			set { this["AuthKeyToken"] = value; }
+		}
+	}
+}
