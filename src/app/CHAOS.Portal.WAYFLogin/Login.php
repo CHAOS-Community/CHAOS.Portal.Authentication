8147346ba79dc390e98468c438518e943acb2ecb
Jacob Poul Richardt <spam@poulrichardt.dk>
2014-06-26 09:31:18 +0200

    Refactored Wayf login.

--- src/app/CHAOS.Portal.WAYFLogin/Login.php
+++ src/app/CHAOS.Portal.WAYFLogin/Login.php
@@ -8,47 +8,76 @@
 	require_once('PortalHelper.php');
 	
 	$error = null;
-	
-	if(!isset($_REQUEST["apiPath"]))
-		$error = "Parameter apiPath not set";
-	else if(!isset($_REQUEST["sessionGuid"]))
-		$error = "Parameter sessionGuid not set";
-	else if(!isset($WayfConfiguration['AuthKeyToken']))
-		$error = "AuthKeyToken not set in configuration";
-	else
+	$status = Login();
+
+	if(isset($_REQUEST["callbackUrl"]))
 	{
+		header('Location: ' . $_REQUEST["callbackUrl"] . "?status=" . $status . ($error == null ? "" : "&error=" . $error), true, 303);
+
+		exit();
+	}
+
+	function Login()
+	{
+		if(!isset($_REQUEST["apiPath"]))
+		{
+			ReportError("Parameter apiPath not set");
+			return 1;
+		}
+		if(!isset($_REQUEST["sessionGuid"]))
+		{
+			ReportError("Parameter sessionGuid not set");
+			return 1;
+		}
+		
+		if(!isset($WayfConfiguration['AuthKeyToken']))
+		{
+			ReportError("AuthKeyToken not set in configuration");
+			return 1;
+		}
+			
 		$simpleSaml = new SimpleSAML_Auth_Simple("Wayf");
 		$simpleSaml->requireAuth();
 		$attributes = $simpleSaml->getAttributes();
-		
+
+		return PortalLogin($attributes);
+	}
+
+	function PortalLogin($attributes)
+	{
+		$encodedAttributes = json_encode($attributes);
+
+		if(!$encodedAttributes)
+		{
+			$error = "Failed to encode wayf attributes";
+			return 1;
+		}
+
 		$helper = new PortalHelper($_REQUEST["apiPath"]);
 		
 		$response = $helper->Call("AuthKey/Login", array('token' => $WayfConfiguration['AuthKeyToken']));
-		
-		if(!($error = $helper->GetError()))
-		{
-			$helper->SetSessionGuid($response->Body->Results[0]->Guid);
-			$response = $helper->Call("Wayf/Login", array('wayfId' => $attributes['eduPersonTargetedID'][0], 'email' => $attributes['mail'][0], 'sessionGuidToAuthenticate' => $_REQUEST["sessionGuid"]));
-		
-			if(!($error = $helper->GetError()))
-			{
-				$helper->Call("WayfProfile/Update", array('userGuid' => $response->Body->Results[0]->Guid,'attributes' => json_encode($attributes)));
 
-				$error = $helper->GetError();
-			}
-		}
-		
-		if(isset($_REQUEST["callbackUrl"]))
-		{
-			$status = ($error == null ? "success" : "failure");
-			
-			/*if(isset(http_build_url))
-				header('Location: ' . http_build_url($_REQUEST["callbackUrl"], array("query" => "status=" . $status), HTTP_URL_JOIN_QUERY), true, 303);
-			else*/
-				header('Location: ' . $_REQUEST["callbackUrl"] . "?status=" . $status . ($error == null ? "" : "&error=" . $error), true, 303);
+		$error = $helper->GetError();
+		if($error != null) return 1;
 
-			exit();
-		}
+		$helper->SetSessionGuid($response->Body->Results[0]->Guid);
+
+		$response = $helper->Call("Wayf/Login", array('attributes' => $encodedAttributes, 'sessionGuidToAuthenticate' => $_REQUEST["sessionGuid"]));
+
+		$error = $helper->GetError();
+		if($error != null) return 2;
+		
+		$helper->Call("WayfProfile/Update", array('userGuid' => $response->Body->Results[0]->Guid, 'attributes' => $encodedAttributes));
+		$error = $helper->GetError();
+		if($error != null) return 1;
+
+		return 0;
+	}
+
+	function ReportError($message)
+	{
+		$error = $message;
+		return false;
 	}
 ?>
 
@@ -56,7 +85,7 @@
     <head>
         <title>Wayf Login</title>
 		<script type="text/javascript">
-			var status = "WayfStatus: <?php print($error == null ? 'success' : 'failure'); ?>";
+			var status = "WayfStatus: <?php $status; ?>";
 			
 			if(window.opener && window.opener.postMessage)
 				window.opener.postMessage(status, "*");
@@ -78,7 +107,7 @@
         <h1>Wayf Login</h1>
         <p>
 		<?php
-			if($error == null)
+			if($status == 0)
 				print("Session authenticated");
 			else
 				print("Error: $error");
